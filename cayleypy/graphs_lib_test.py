3b3fa171602dd59a4c676042c614b7279b8e6063
Arsenii Sychev <57996364+messoem@users.noreply.github.com>
2025-09-27 18:35:07 +0300

    Koltsov3 (#138)
    * Update graphs_lib.py
    
    * Update graphs_lib_test.py
    
    * Update graphs_lib.py
    
    * Update graphs_lib_test.py
    
    * Update graphs_lib.py
    
    * Update graphs_lib_test.py
    
    * Update graphs_lib.py
    
    * Update graphs_lib_test.py
    
    * Update graphs_lib.py
    
    * Update graphs_lib.py
    
    * Update graphs_lib.py
    
    * Update graphs_lib.py
    
    * Update pyproject.toml
    
    * restore comments
    
    * add more tests
    
    ---------
    
    Co-authored-by: Dmytro Fedoriaka <fedimser@gmail.com>

--- cayleypy/graphs_lib_test.py
+++ cayleypy/graphs_lib_test.py
@@ -392,6 +392,19 @@ def test_sheveleva2():
     assert graph2.generators == [[1, 0, 3, 2, 4, 7, 6, 5], [0, 2, 1, 4, 5, 6, 3, 7]]
 
 
+def test_koltsov3():
+    graph = PermutationGroups.koltsov3(6, 2)
+    assert graph.generators == [[1, 0, 3, 2, 5, 4], [0, 2, 1, 4, 3, 5], [0, 4, 3, 2, 1, 5]]
+    graph2 = PermutationGroups.koltsov3(10, 2, 3, 1)
+    assert graph2.generators == [
+        [1, 0, 3, 2, 5, 4, 7, 6, 9, 8],
+        [0, 2, 1, 4, 3, 6, 5, 8, 7, 9],
+        [0, 1, 2, 6, 5, 4, 3, 7, 8, 9],
+    ]
+    assert graph2.generator_names == ["I", "K", "S"]
+    assert graph2.name == "koltsov3-n10-k3"
+
+
 def test_heisenberg():
     graph1 = MatrixGroups.heisenberg()
     assert graph1.name == "heisenberg-3-ic"
